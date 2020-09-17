b9259d0236b8d566a80da54d04a9e978a79cc633
Arnheidur <arnheidur19@ru.is>
2020-09-17 14:50:56 +0000

    bætti við föllum

--- TileTraveller.py
+++ TileTraveller.py
@@ -7,6 +7,7 @@
 #  prenta victory 
 #
 
+# fall fyrir tile_1.1
 def tile_1.1():
    
     if direction == "n" or direction == "N":
@@ -15,9 +16,34 @@ def tile_1.1():
     else:
         return print("Not a valid direction!")   
 
+# fall fyrir tile_1.2
+def tile_1.2():
+    
+    if direction == "n" or direction == "N":
+        current_tile = 1.3
+    elif direction == "e" or direction == "E":
+        current_tile = 2.2
+    elif direction == "s" or direction == "S":
+        current_tile = 1.1
+    else:
+        return print("Not a valid direction!")
+
+# fall fyrir tile_1.3
+def tile_1.3():
+
+    if direction == "e" or direction == "E":
+        current_tile = 2.3
+    elif direction == "s" or direction == "S":
+        current_tile = 1.2
+    else:
+        return print("Not a valid direction!")
+
+
+
 
 current_tile = 1.1
 
+# skipanir ef við erum stödd á tile_1.1 sem kallar í viðeigandi fall
 if current_tile = 1.1:
 
     print("You can travel (N)orth.")
@@ -26,35 +52,32 @@ if current_tile = 1.1:
 
     print(tile_1.1())
 
+# skipanir ef við erum stödd á tile_1.2 sem kallar í viðeigandi fall
+if current_tile == 1.2:
 
+    print("You can travel (N)orth or (E)ast or (S)outh.")
 
+    direction = input("Direction: ")
+
+    print(tile_1.2())
+
+# skipanir ef við erum stödd á tile_1.3 sem kallar í viðeigandi fall
+ if current_tile == 1.3:
+
+    print("You can travel (E)ast or (S)outh.")
+
+    direction = input("Direction: ")
+
+    print(tile_1.3())
 
 
 
 if current_tile == 1.1:
 
         
-    elif current_tile == 1.2:
-        print("You can travel (N)orth or (E)ast or (S)outh.")
-        direction = input("Direction: ")
-        if direction == "n" or direction == "N":
-            current_tile = 1.3
-        elif direction == "e" or direction == "E":
-            current_tile = 2.2
-        elif direction == "s" or direction == "S":
-            current_tile = 1.1
-        else:
-            print("Not a valid direction!")
+   
 
-    elif current_tile == 1.3:
-        print("You can travel (E)ast or (S)outh.")
-        direction = input("Direction: ")
-        if direction == "e" or direction == "E":
-            current_tile = 2.3
-        elif direction == "s" or direction == "S":
-            current_tile = 1.2
-        else:
-            print("Not a valid direction!")
+   
 
     elif current_tile == 2.1:
         print("You can travel (N)orth.")
